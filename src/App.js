509d75eb899749cbe137bb9c58e8af0667f0e4fd
Kiran Gaonkar <kirangaonkar@Triconinfotech.com>
2018-12-13 21:54:58 +0530

    events

--- src/App.js
+++ src/App.js
@@ -3,16 +3,31 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    name: 'techsith'
+  }
+
+  changeName = (newName) => {
+    this.setState({
+      name: newName
+    });
+  }
+
+  changeNameFromInput = (event) => {
+    this.setState({
+      name: event.target.value
+    });
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Learn React reactively</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <br /><br />
+        <button onClick={() => this.changeName('Awsemo tech')}>Change state using a non State</button>
+        <button onClick={this.changeName.bind(this, 'Awsemo tech:)')}>Change State using bind</button>
+        <br /><br />
+        <input type="text" onChange={this.changeNameFromInput} value={this.state.name} />
+        <br /><br />
+        <div>{this.state.name}</div>
       </div>
     );
   }
