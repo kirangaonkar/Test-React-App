b64b65fa0138a3b5be8b8fc24f09d1a85fa3fa35
Kiran Gaonkar <kirangaonkar@Triconinfotech.com>
2018-12-14 17:38:47 +0530

    Router in depth

--- src/App.js
+++ src/App.js
@@ -1,19 +1,73 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Link, NavLink, Redirect, Prompt } from 'react-router-dom';
+import Route from 'react-router-dom/Route'
 
+
+const User = (params) => {
+  return (<h1> Welcome User {params.username}</h1>);
+}
 class App extends Component {
+
+  state = {
+    loggedIn: false
+  }
+
+  loginHandle = () => {
+    this.setState(prevState => ({
+      loggedIn: !prevState.loggedIn
+    }));
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Learn React reactively</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div className="App">
+          <ul>
+            <li>
+              <NavLink to="/home" exact activeStyle={
+                { color: 'green' }
+              } > Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/about" exact activeStyle={
+                { color: 'green' }
+              } >About</NavLink>
+            </li>
+            <li>
+              <NavLink to="/user/John" exact activeStyle={
+                { color: 'green' }
+              } >User</NavLink>
+            </li>
+          </ul>
+
+          <Prompt
+            when={!this.state.loggedIn}
+            message={(location) => {
+              return location.pathname.startsWith('/user') ? "Are you sure?" : true;
+            }} />
+
+          <input type="button" value={this.state.loggedIn ? 'Log out' : 'log in'} onClick={this.loginHandle.bind(this)}></input>
+          <Route path="/home" exact activeStyle={
+            { color: 'green' }
+          } exact render={
+            () => {
+              return (<h1>First Router</h1>);
+            }
+          } />
+          <Route path="/about" exact strict render={
+            () => {
+              return (<h1>About</h1>);
+            }
+          } />
+
+          <Route path="/user/:username" render={({ match }) => {
+            return this.state.loggedIn ? (<User username={match.params.username} />) : (<Redirect to="/home" />)
+          }
+          } />
+
+
+        </div>
+      </Router >
     );
   }
 }
