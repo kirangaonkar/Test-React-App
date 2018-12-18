f9f5748dee8e0d73a2ed96b5720862b6e1897f50
Kiran Gaonkar <kirangaonkar@Triconinfotech.com>
2018-12-18 11:25:21 +0530

    login page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+export const InputField = (props) => {
+  return (
+    <div className="input">
+      <input placeholder={props.placeholder} onChange={props.onChange} type={props.type} value={props.val} />
+    </div>
+  );
+}
 class App extends Component {
+  state = {
+    userName: "",
+    password: ""
+  }
+
+  onChange = (value) => {
+    this.setState({
+      userName: value.target.value
+    });
+    console.log(value.target.value);
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Learn React reactively</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="login-container">
+          <div className="login-form">
+            <InputField placeholder="Username" onChange={this.onChange} type="text" val={this.state.userName} />
+            <InputField placeholder="Password" onChange={this.onChange} type="password" val={this.state.password} />
+          </div>
+        </div>
       </div>
     );
   }
